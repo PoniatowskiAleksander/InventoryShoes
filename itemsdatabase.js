d012fff24eeb60c8523b8e1beac27ece668dbc22
Aleksander Poniatowski <clakoja@gmail.com>
2022-04-17 01:49:15 +0200

    Items Database display DONE

--- itemsdatabase.js
+++ itemsdatabase.js
@@ -22,7 +22,50 @@ document.querySelector('.itemsdatabase .beforeLoadDiv button').addEventListener(
             document.querySelector('.itemsdatabase').innerHTML = '<div class="itemsdatabaseMainDiv"></div>';
             for (i of shoes) {
                 console.log(i);
-                
+                let itemDiv = document.createElement('div');
+                itemDiv.classList.add('item');
+
+                // adding item info to itemDiv
+                //img
+                let itemImg = document.createElement('img');
+                itemImg.setAttribute('src', i.ImgLink);
+                let itemImgWrap = document.createElement('a');
+                itemImgWrap.setAttribute('href', i.stockxLink);
+                itemImgWrap.setAttribute('target', '_blank');
+                // itemImg.addEventListener('click', function() {
+                //     let temp = document.createElement('a');
+                //     temp.setAttribute('href', link);
+                //     temp.click();
+                // })
+                itemImgWrap.appendChild(itemImg);
+                itemDiv.appendChild(itemImgWrap);
+
+                //item name
+                let itemName = document.createElement('div');
+                itemName.classList.add('itemNameId');
+                let itemNameHeader = document.createElement('header');
+                itemNameHeader.innerHTML = 'Item Name';
+                let itemNameValue = document.createElement('p');
+                itemNameValue.innerHTML = i['Item name'];
+                itemName.appendChild(itemNameHeader);
+                itemName.appendChild(document.createElement('hr'));
+                itemName.appendChild(itemNameValue);
+
+                // item id
+                let itemId = document.createElement('div');
+                itemId.classList.add('itemNameId');
+                let itemIdHeader = document.createElement('header');
+                itemIdHeader.innerHTML = 'ID';
+                let itemIdValue = document.createElement('p');
+                itemIdValue.innerHTML = i.ID;
+                itemId.appendChild(itemIdHeader);
+                itemId.appendChild(document.createElement('hr'));
+                itemId.appendChild(itemIdValue);
+
+                itemDiv.append(itemName);
+                itemDiv.append(itemId);
+
+                document.querySelector('.itemsdatabaseMainDiv').append(itemDiv);
 
             }
         })
